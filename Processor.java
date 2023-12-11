e0215c44c153b9b20c5559a43bedcb4fea2491b8
luis-borges <kdegrize@gmail.com>
2023-12-11 16:21:09 +0100

    exple pour parcourir de AST

--- Processor.java
+++ Processor.java
@@ -23,11 +23,27 @@ public class Processor {
         CommonTree tree = (CommonTree)ret.getTree();
 
         System.out.println(tree.toStringTree());
-        //System.out.println(ast);
+        printTree(tree, 1);
     }
     static final TreeAdaptor adaptor = new CommonTreeAdaptor() {
         public Object create(Token payload) {
             return new CommonTree(payload);
         }
     };
+
+    public static void printTree(CommonTree t, int indent) {
+        if ( t != null ) {
+            StringBuffer sb = new StringBuffer(indent);
+
+            if (t.getParent() == null){
+                System.out.println(sb.toString() + t.getText().toString());
+            }
+            for ( int i = 0; i < indent; i++ )
+                sb = sb.append("   ");
+            for ( int i = 0; i < t.getChildCount(); i++ ) {
+                System.out.println(sb.toString() + t.getChild(i).toString());
+                printTree((CommonTree)t.getChild(i), indent+1);
+            }
+        }
+    }
 }
